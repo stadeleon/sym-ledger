fd99d733450e7c51f995db07d5c7d4f282cc9d03
Leonid Derevianko <leo.mrakobes@gmail.com>
2025-02-18 03:44:42 +0100

    Basic symfony project initialized

---
+++ docker/php/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ -z "$(ls -A /var/www/app)" ]; then
+    echo "Dir /var/www/app is empty. Downloading Symfony skeleton..."
+    composer create-project symfony/skeleton:"^6.4" /var/www/app
+    composer require symfony/webapp-pack
+fi
+
+exec php-fpm
